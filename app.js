1baef29ba5165a2513d859e015db2d26f51ff084
sahjarkhan <sahjar@headerlabs.com>
2021-11-07 21:13:26 +0530

    a

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
     res.send('Updated....')
 });
 
 const port =process.env.port || 3000;
-
-
 app.listen(port, () => {
     console.log('port',port)
 })
